508727359f3ee111464b415a1927b88a17a9c336
Fergus Farrell <fergus.farrell@comrad.co.nz>
2021-07-11 13:30:30 +1200

    Added styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,32 @@
 <script>
 import BookItem from "./BookItem.svelte";
 
-	let searchTerm;
-	let books;
-
-	const handleSearch = () => {
-		books = searchBooks();
-	}
-
 	const searchBooks = async () => {
 		return await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&maxResults=5`)
 			.then(res => res.json());
 	}
+
+	let searchTerm = 'this is going to hurt';
+	let books = searchBooks();
+
+	const handleSearch = (e) => {
+		e.preventDefault();
+		books = searchBooks();
+	};
 </script>
 
 <main>
-	<input bind:value={searchTerm} placeholder="Search by title or author">
-	<button on:click={handleSearch}>Search</button>
+	<form>
+		<input bind:value={searchTerm} placeholder="Search by title or author">
+		<button on:click={handleSearch} type="submit">Search</button>
+	</form>
 
 	{#await books}
 		<p>searching...</p>
 	{:then bookList}
 		{#if bookList && bookList.items}
-			{#each bookList.items as { volumeInfo: { title, subtitle, authors } }}
-				<BookItem title={title} subtitle={subtitle} authors={authors} />
+			{#each bookList.items as { selfLink, volumeInfo: { title, subtitle, authors } }}
+				<BookItem title={title} subtitle={subtitle} authors={authors} link={selfLink} />
 			{/each}
 		{/if}
 	{/await}	
@@ -35,6 +38,7 @@ import BookItem from "./BookItem.svelte";
 		max-width: 240px;
 		margin: 0 auto;
 	}
+
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
--- src/BookItem.svelte
+++ src/BookItem.svelte
@@ -1,34 +1,74 @@
 <script>
+import App from "./App.svelte";
+
   export let title;
   export let subtitle;
   export let authors;
+  export let link;
+
+  let adding = false;
 
   const handleBookSelect = () => {
+    adding = true;
     fetch('https://us-central1-test-database-200da.cloudfunctions.net/createPage', {
       method: 'POST',
       body: JSON.stringify({
         title,
         author: authors.join(', '),
+        subtitle,
+        link
       }),
-    });
+    }).finally(() => {
+      adding = false;
+    })
   };
     
 </script>
 
 <div class="book-item" on:click={handleBookSelect}>
-  <p>{title}</p>
-  <p class="subtitle">{subtitle}</p>
-  <p class="subtitle">{authors[0]}</p>
+  {#if adding}
+    Adding {title}...
+  {:else}
+    <div class="book-title">
+      <h4>{title}</h4>
+      {#if subtitle}
+        <p class="subtitle">{subtitle}</p>
+      {/if}
+    </div>
+    {#if authors}
+      <p class="subtitle">By {authors.join(', ')}</p>
+    {/if}
+  {/if} 
 </div>
 
 <style>
   .book-item {
-    border: 1px grey solid;
-    padding: 1em;
-    margin: 1em;
+    border: 1px #ccc solid;
+    padding: 0.7em;
+    margin: 1em 0;
     cursor: pointer;
+    transition: 0.2s ease;
   }
 
+  .book-item:hover {
+    background-color: #ccc;
+  }
+
+  .book-title {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  h4 {
+    padding: 0;
+    margin: 0;
+  }
+
+  p {
+    padding: 0;
+    margin: 0;
+  }
   .subtitle {
 		color: grey;
 		font-size: small;
